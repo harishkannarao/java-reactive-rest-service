fcd20765c2bab46a7c8c7f39fa0907dc2728592e
harishkannarao <harishkannarao@users.noreply.github.com>
2021-08-03 21:20:51 +0100

    Simplifying integration test

--- src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/integration/JavaReactiveRestServiceApplicationTests.java
+++ src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/integration/JavaReactiveRestServiceApplicationTests.java
@@ -2,17 +2,21 @@ package com.harishkannarao.java.spring.rest.javareactiverestservice.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 class JavaReactiveRestServiceApplicationTests extends AbstractBaseIntegrationTest {
 
-	@Test
-	void contextLoads() {
-		webTestClient()
-				.get()
-				.uri("/")
-				.accept(MediaType.APPLICATION_JSON)
-				.exchange()
-				.expectStatus().isNotFound();
-	}
+    @Test
+    void contextLoads() {
+        webTestClient()
+                .get()
+                .uri("/")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.status").isEqualTo(404)
+                .jsonPath("$.error").isEqualTo("Not Found");
+    }
 
 }
