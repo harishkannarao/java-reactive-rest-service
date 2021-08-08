be1662c5c5f3e7d968a1091271e9d71c33af410f
harishkannarao <harishkannarao@users.noreply.github.com>
2021-08-08 08:42:56 +0100

    Adding test for unique index constraint

--- src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/integration/CustomerRepositoryIntegrationTest.java
+++ src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/integration/CustomerRepositoryIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SuppressWarnings("ConstantConditions")
 public class CustomerRepositoryIntegrationTest extends AbstractBaseIntegrationTest {
@@ -26,5 +27,20 @@ public class CustomerRepositoryIntegrationTest extends AbstractBaseIntegrationTe
         assertThat(result.getId()).isEqualTo(input.getId());
         assertThat(result.getFirstName()).isEqualTo(input.getFirstName());
         assertThat(result.getLastName()).isEqualTo(input.getLastName());
+    }
+
+    @Test
+    void cannotCreateMultipleCustomersWithSameId() {
+        Customer input = new Customer(
+                UUID.randomUUID(),
+                "firstName",
+                "lastName"
+        );
+        CustomerRepository underTest = getBean(CustomerRepository.class);
+        UUID createdId = underTest.createCustomer(input).block();
+        assertThat(createdId).isEqualTo(input.getId());
+
+        assertThatThrownBy(() -> underTest.createCustomer(input).block())
+                .hasMessageContaining("duplicate key value violates unique constraint \"unique_index_jsonb_id\"");
     }
 }
