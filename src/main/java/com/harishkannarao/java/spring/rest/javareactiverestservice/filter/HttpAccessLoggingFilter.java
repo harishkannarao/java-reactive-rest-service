6af38b97bffd2c0739b5c29bc10410fdc5964173
harishkannarao <harishkannarao@users.noreply.github.com>
2021-08-13 09:05:03 +0100

    Using Custom class for log entries

--- src/main/java/com/harishkannarao/java/spring/rest/javareactiverestservice/filter/HttpAccessLoggingFilter.java
+++ src/main/java/com/harishkannarao/java/spring/rest/javareactiverestservice/filter/HttpAccessLoggingFilter.java
@@ -1,23 +1,16 @@
 package com.harishkannarao.java.spring.rest.javareactiverestservice.filter;
 
-import net.logstash.logback.argument.StructuredArguments;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.io.Serializable;
-import java.util.Map;
-
-import static java.util.Map.entry;
-import static java.util.Map.ofEntries;
-import static net.logstash.logback.argument.StructuredArguments.entries;
+import static net.logstash.logback.argument.StructuredArguments.fields;
 import static org.springframework.web.server.ServerWebExchange.LOG_ID_ATTRIBUTE;
 
 @Component
@@ -31,24 +24,72 @@ public class HttpAccessLoggingFilter implements WebFilter {
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         exchange.getAttributes().put(REQUEST_START_TIME_ATTRIBUTE, System.currentTimeMillis());
         exchange.getResponse().beforeCommit(() -> {
-            Integer statusCode = exchange.getResponse().getRawStatusCode();
-            String requestId = exchange.getRequiredAttribute(LOG_ID_ATTRIBUTE);
-            String method = exchange.getRequest().getMethodValue();
-            String pathWithApplication = exchange.getRequest().getPath().pathWithinApplication().value();
-            String queryParams = exchange.getRequest().getQueryParams().toString();
-            long responseTime = System.currentTimeMillis() - (Long) exchange.getRequiredAttribute(REQUEST_START_TIME_ATTRIBUTE);
-            Map<String, ? extends Serializable> logEntries = ofEntries(
-                    entry("status", statusCode),
-                    entry("requestId", requestId),
-                    entry("method", method),
-                    entry("path", pathWithApplication),
-                    entry("queryParams", queryParams),
-                    entry("responseTimeInMillis", responseTime)
+            AccessLogEntries accessLogEntries = new AccessLogEntries(
+                    exchange.getResponse().getRawStatusCode(),
+                    exchange.getRequiredAttribute(LOG_ID_ATTRIBUTE),
+                    exchange.getRequest().getMethodValue(),
+                    exchange.getRequest().getPath().pathWithinApplication().value(),
+                    exchange.getRequest().getQueryParams().toString(),
+                    System.currentTimeMillis() - (Long) exchange.getRequiredAttribute(REQUEST_START_TIME_ATTRIBUTE)
             );
-            logger.info("Access Log {}", entries(logEntries));
+            logger.info("Access Log {}", fields(accessLogEntries));
             return Mono.empty();
         });
         return chain.filter(exchange);
+    }
+
+    public static class AccessLogEntries {
+        private final Integer status;
+        private final String requestId;
+        private final String method;
+        private final String path;
+        private final String queryParams;
+        private final Long responseTimeMs;
+
+        public AccessLogEntries(Integer status, String requestId, String method, String path, String queryParams, Long responseTimeMs) {
+            this.status = status;
+            this.requestId = requestId;
+            this.method = method;
+            this.path = path;
+            this.queryParams = queryParams;
+            this.responseTimeMs = responseTimeMs;
+        }
+
+        public Integer getStatus() {
+            return status;
+        }
+
+        public String getRequestId() {
+            return requestId;
+        }
+
+        public String getMethod() {
+            return method;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public String getQueryParams() {
+            return queryParams;
+        }
+
+        public Long getResponseTimeMs() {
+            return responseTimeMs;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "status=" + status +
+                    ", requestId='" + requestId + '\'' +
+                    ", method='" + method + '\'' +
+                    ", path='" + path + '\'' +
+                    ", queryParams='" + queryParams + '\'' +
+                    ", responseTimeMs=" + responseTimeMs +
+                    '}';
+        }
     }
 
 }
