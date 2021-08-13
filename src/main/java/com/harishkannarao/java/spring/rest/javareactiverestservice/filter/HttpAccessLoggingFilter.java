8225e793222cb766d159aac298f3fe2019d3cbbe
harishkannarao <harishkannarao@users.noreply.github.com>
2021-08-13 22:36:48 +0100

    Json structured logging using logstash

--- src/main/java/com/harishkannarao/java/spring/rest/javareactiverestservice/filter/HttpAccessLoggingFilter.java
+++ src/main/java/com/harishkannarao/java/spring/rest/javareactiverestservice/filter/HttpAccessLoggingFilter.java
@@ -10,6 +10,12 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
 import static net.logstash.logback.argument.StructuredArguments.fields;
 import static org.springframework.web.server.ServerWebExchange.LOG_ID_ATTRIBUTE;
 
@@ -17,12 +23,14 @@ import static org.springframework.web.server.ServerWebExchange.LOG_ID_ATTRIBUTE;
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
 public class HttpAccessLoggingFilter implements WebFilter {
     public static final String REQUEST_START_TIME_ATTRIBUTE = "REQUEST_START_TIME";
+    public static final String REQUEST_TIMESTAMP_ATTRIBUTE = "REQUEST_TIMESTAMP";
 
     private final Logger logger = LoggerFactory.getLogger(HttpAccessLoggingFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         exchange.getAttributes().put(REQUEST_START_TIME_ATTRIBUTE, System.currentTimeMillis());
+        exchange.getAttributes().put(REQUEST_TIMESTAMP_ATTRIBUTE, OffsetDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MILLIS).format(DateTimeFormatter.ISO_DATE_TIME));
         exchange.getResponse().beforeCommit(() -> {
             AccessLogEntries accessLogEntries = new AccessLogEntries(
                     exchange.getResponse().getRawStatusCode(),
@@ -30,9 +38,11 @@ public class HttpAccessLoggingFilter implements WebFilter {
                     exchange.getRequest().getMethodValue(),
                     exchange.getRequest().getPath().pathWithinApplication().value(),
                     exchange.getRequest().getQueryParams().toString(),
-                    System.currentTimeMillis() - (Long) exchange.getRequiredAttribute(REQUEST_START_TIME_ATTRIBUTE)
+                    System.currentTimeMillis() - (Long) exchange.getRequiredAttribute(REQUEST_START_TIME_ATTRIBUTE),
+                    exchange.getRequiredAttribute(REQUEST_TIMESTAMP_ATTRIBUTE),
+                    OffsetDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.MILLIS).format(DateTimeFormatter.ISO_DATE_TIME)
             );
-            logger.info("Access Log {}", fields(accessLogEntries));
+            logger.info("{}", fields(accessLogEntries));
             return Mono.empty();
         });
         return chain.filter(exchange);
@@ -45,14 +55,18 @@ public class HttpAccessLoggingFilter implements WebFilter {
         private final String path;
         private final String queryParams;
         private final Long responseTimeMs;
+        private final String requestTimestamp;
+        private final String responseTimestamp;
 
-        public AccessLogEntries(Integer status, String requestId, String method, String path, String queryParams, Long responseTimeMs) {
+        public AccessLogEntries(Integer status, String requestId, String method, String path, String queryParams, Long responseTimeMs, String requestTimestamp, String responseTimestamp) {
             this.status = status;
             this.requestId = requestId;
             this.method = method;
             this.path = path;
             this.queryParams = queryParams;
             this.responseTimeMs = responseTimeMs;
+            this.requestTimestamp = requestTimestamp;
+            this.responseTimestamp = responseTimestamp;
         }
 
         public Integer getStatus() {
@@ -79,15 +93,25 @@ public class HttpAccessLoggingFilter implements WebFilter {
             return responseTimeMs;
         }
 
+        public String getRequestTimestamp() {
+            return requestTimestamp;
+        }
+
+        public String getResponseTimestamp() {
+            return responseTimestamp;
+        }
+
         @Override
         public String toString() {
-            return "{" +
+            return "AccessLogEntries{" +
                     "status=" + status +
                     ", requestId='" + requestId + '\'' +
                     ", method='" + method + '\'' +
                     ", path='" + path + '\'' +
                     ", queryParams='" + queryParams + '\'' +
                     ", responseTimeMs=" + responseTimeMs +
+                    ", requestTimestamp='" + requestTimestamp + '\'' +
+                    ", responseTimestamp='" + responseTimestamp + '\'' +
                     '}';
         }
     }
