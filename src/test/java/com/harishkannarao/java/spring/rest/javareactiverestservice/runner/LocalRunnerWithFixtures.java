4422351d5c100f2752f5d4cf81905af11bc361b6
harishkannarao <harishkannarao@users.noreply.github.com>
2021-08-14 23:07:24 +0100

    Creating customer fixtures during local run

--- src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/runner/LocalRunnerWithFixtures.java
+++ src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/runner/LocalRunnerWithFixtures.java
@@ -1,15 +1,21 @@
 package com.harishkannarao.java.spring.rest.javareactiverestservice.runner;
 
+import com.harishkannarao.java.spring.rest.javareactiverestservice.fixture.CustomerFixtures;
+import com.harishkannarao.java.spring.rest.javareactiverestservice.model.Customer;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.harishkannarao.java.spring.rest.javareactiverestservice.client.Clients.customerApiClient;
+
 public class LocalRunnerWithFixtures {
 
     public static void main(String[] args) {
         PostgresTestRunner.start();
+
         List<String> finalArgs = Stream.of(
                         getPostgresTestProperties(),
                         getSpringTestProperties(),
@@ -17,6 +23,15 @@ public class LocalRunnerWithFixtures {
                 ).flatMap(Collection::stream)
                 .collect(Collectors.toList());
         SpringBootTestRunner.start(finalArgs);
+
+        createCustomerFixtures();
+    }
+
+    private static void createCustomerFixtures() {
+        Customer customer1 = CustomerFixtures.randomCustomer();
+        Customer customer2 = CustomerFixtures.randomCustomer();
+        customerApiClient().createMultiple(List.of(customer1, customer2))
+                        .expectStatus().isOk();
     }
 
     private static List<String> getPostgresTestProperties() {
