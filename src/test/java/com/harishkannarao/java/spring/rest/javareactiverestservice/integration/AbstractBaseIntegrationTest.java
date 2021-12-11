5b892c91a7eca6269adc0b84ac5b60df2ed9132e
harishkannarao <harishkannarao@users.noreply.github.com>
2021-12-11 22:53:12 +0000

    Simplifying properties injection

--- src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/integration/AbstractBaseIntegrationTest.java
+++ src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/integration/AbstractBaseIntegrationTest.java
@@ -2,16 +2,12 @@ package com.harishkannarao.java.spring.rest.javareactiverestservice.integration;
 
 import com.harishkannarao.java.spring.rest.javareactiverestservice.json.JsonUtil;
 import com.harishkannarao.java.spring.rest.javareactiverestservice.runner.MockServerTestRunner;
-import com.harishkannarao.java.spring.rest.javareactiverestservice.runner.PostgresTestRunner;
-import com.harishkannarao.java.spring.rest.javareactiverestservice.runner.TestSupportExtension;
 import com.harishkannarao.java.spring.rest.javareactiverestservice.runner.SpringBootTestRunner;
+import com.harishkannarao.java.spring.rest.javareactiverestservice.runner.TestSupportExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.harishkannarao.java.spring.rest.javareactiverestservice.client.Clients.customerApiClient;
 
@@ -28,41 +24,10 @@ public abstract class AbstractBaseIntegrationTest {
         customerApiClient().deleteAll().expectStatus().isNoContent();
     }
 
-    private List<String> getPostgresTestProperties() {
+    protected List<String> getIntegrationTestProperties() {
         return List.of(
-                "--postgresql-datasource.timeout=10",
-                "--postgresql-datasource.host=" + PostgresTestRunner.getHost(),
-                "--postgresql-datasource.port=" + PostgresTestRunner.getPort(),
-                "--postgresql-datasource.database=" + PostgresTestRunner.getUsername(),
-                "--postgresql-datasource.username=" + PostgresTestRunner.getUsername(),
-                "--postgresql-datasource.password=" + PostgresTestRunner.getPassword(),
-                "--spring.flyway.url=" + PostgresTestRunner.getJdbcUrl(),
-                "--spring.flyway.user=" + PostgresTestRunner.getUsername(),
-                "--spring.flyway.password=" + PostgresTestRunner.getPassword()
+                "--spring.profiles.active=int-test"
         );
-    }
-
-    private List<String> getMockServerTestProperties() {
-        return List.of(
-                "--order-service.base-url=" + MockServerTestRunner.getUrl(),
-                "--order-service.timeout-seconds=5"
-        );
-    }
-
-    private List<String> getSpringTestProperties() {
-        return List.of(
-                "--spring.profiles.active=int-test",
-                "--server.port=0"
-        );
-    }
-
-    private List<String> getIntegrationTestProperties() {
-        return Stream.of(
-                        getPostgresTestProperties(),
-                        getMockServerTestProperties(),
-                        getSpringTestProperties()
-                ).flatMap(Collection::stream)
-                .collect(Collectors.toList());
     }
 
     protected <T> T getBean(Class<T> clazz) {
--- src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/runner/TestSupportExtension.java
+++ src/test/java/com/harishkannarao/java/spring/rest/javareactiverestservice/runner/TestSupportExtension.java
@@ -1,13 +1,28 @@
 package com.harishkannarao.java.spring.rest.javareactiverestservice.runner;
 
 import org.junit.jupiter.api.extension.*;
+import org.springframework.util.SocketUtils;
 
 public class TestSupportExtension implements BeforeAllCallback, BeforeEachCallback, AfterAllCallback, AfterEachCallback {
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) {
         PostgresTestRunner.start();
+        System.setProperty("postgresql-datasource.timeout", "10");
+        System.setProperty("postgresql-datasource.host", PostgresTestRunner.getHost());
+        System.setProperty("postgresql-datasource.port", String.valueOf(PostgresTestRunner.getPort()));
+        System.setProperty("postgresql-datasource.database", PostgresTestRunner.getUsername());
+        System.setProperty("postgresql-datasource.username", PostgresTestRunner.getUsername());
+        System.setProperty("postgresql-datasource.password", PostgresTestRunner.getPassword());
+        System.setProperty("spring.flyway.url", PostgresTestRunner.getJdbcUrl());
+        System.setProperty("spring.flyway.user", PostgresTestRunner.getUsername());
+        System.setProperty("spring.flyway.password", PostgresTestRunner.getPassword());
+
         MockServerTestRunner.start();
+        System.setProperty("order-service.base-url", MockServerTestRunner.getUrl());
+        System.setProperty("order-service.timeout-seconds", "5");
+
+        System.setProperty("server.port", String.valueOf(SocketUtils.findAvailableTcpPort()));
     }
 
     @Override
